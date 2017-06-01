bc8cf5a4db0991d3bc628aa9913399a2c2bb05bd
Kurt Junghanns <tboonx@googlemail.com>
2017-06-01 11:01:24 +0200

    [master] Added migration code for user-service

---
+++ src/lib_userservice/configuration.js
@@ -0,0 +1,49 @@
+/* This module is used for configurating the mongodb connection*/
+'use strict';
+
+const co = require('./common.js');
+
+let host = 'localhost';
+//read mongo URL from /etc/hosts
+const fs = require('fs');
+try {
+    const lines = fs.readFileSync('/etc/hosts').toString().split('\n');
+    lines.filter((line) => line.includes('mongodb')).forEach((line) => {
+        const entries = line.split(' ');
+        host = entries[entries.length - 1];
+        console.log('Using ' + host + ' as database host.');
+    });
+} catch (e) {
+    console.log('Exception: Windows or no read rights to read /etc/hosts (bad)');
+}
+//read mongo URL from ENV
+host = (!co.isEmpty(process.env.DATABASE_URL)) ? process.env.DATABASE_URL : host;
+if (host !== 'localhost')
+    console.log('Using ' + host + ' as database host.');
+
+let port = 27017;
+//read mongo port from ENV
+if (!co.isEmpty(process.env.DATABASE_PORT)) {
+    port = process.env.DATABASE_PORT;
+    console.log('Using ' + port + ' as database port.');
+}
+
+//JWT serial
+let JWTSerial = '69aac7f95a9152cd4ae7667c80557c284e413d748cca4c5715b3f02020a5ae1b';
+if (!co.isEmpty(process.env.JWT_SERIAL)) {
+    JWTSerial = process.env.JWT_SERIAL;
+}
+
+module.exports = {
+    MongoDB: {
+        PORT: port,
+        HOST: host,
+        NS: 'local',
+        SLIDEWIKIDATABASE: 'slidewiki'
+    },
+    JWT: {
+        SERIAL: JWTSerial,
+        HEADER: '----jwt----',
+        ALGORITHM: 'HS512'
+    }
+};
