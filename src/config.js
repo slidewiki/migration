61e19db717f08a6ef8d251257e0215b272713809
Kurt Junghanns <tboonx@googlemail.com>
2017-07-17 10:38:20 +0200

    [Using_one_config_file] Deleted duplicated config file and used it. updated helper.js and config.js.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@
 
 let Config = {
     slidewiki : 'mongodb://mongodb:27017/slidewiki',
+    slidewiki_db_name: 'slidewiki',
     slidewiki_stable : 'mongodb://mongodb:27017/stable',
+    slidewiki_stable_db_name: 'stable',
     MysqlConnection : {
         user: 'root',
         password: 'linuxisgreat',
