dbe081941fe9090ed8a8d0a8488710eaa1a5d068
Ben Wulff <benjamin.wulff.de@ieee.org>
2017-07-17 15:03:11 +0200

    improved production script

--- run_production_migration.sh
+++ run_production_migration.sh
@@ -2,22 +2,13 @@
 
 PRJ=$1
 
-docker run \
-  --name mysql
-  --network=$PRJ\_front-trier \
+docker run --name mysql \
+        --network=$PRJ\_back-trier \
   -e MYSQL_ROOT_PASSWORD=linuxisgreat \
   -e MYSQL_DATABASE=slidewiki \
-  -v sql_dump:/docker-entrypoint-initdb.d
+  -v $PWD/sql_dump:/docker-entrypoint-initdb.d \
   -d mysql
 
-docker run \
-  --name slidewiki_migration
-  --network=$PRJ\_front-trier \
-  --link mysql
-  --link $PRJ\_mongodb_1
-  -it slidewiki/migration:latest
+docker logs -f mysql
 
-
-
-
-  
+docker run --name slidewiki_migration --network=$PRJ\_back-trier --link mysql --link $PRJ\_mongodb_1 -it slidewiki/migration:latest /bin/bash
